e0d9b54b218fe747d9b95afad90ecdf1942a92b4
Juan Lara <max_lara3005@hotmail.com>
2023-07-09 12:52:13 -0600

    first commit

---
+++ src/main/java/com/platzi/pizza/persistence/entity/CustomerEntity.java
@@ -0,0 +1,30 @@
+package com.platzi.pizza.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="customer")
+public class CustomerEntity {
+    @Id
+    @Column(name = "id_customer", nullable = false, length = 15)
+    private Integer idCustomer;
+
+    @Column(nullable = false, length = 60)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String address;
+
+    @Column(nullable = false, length = 50)
+    private  String email;
+
+    @Column(name = "phone_number", nullable = false,  length = 20)
+    private String phoneNumber;
+}
---
+++ src/main/java/com/platzi/pizza/persistence/entity/OrderItemEntity.java
@@ -0,0 +1,36 @@
+package com.platzi.pizza.persistence.entity;
+
+import com.platzi.pizza.persistence.entity.ids.OrderItemId;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+@NoArgsConstructor
+@IdClass(OrderItemId.class)
+public class OrderItemEntity {
+    @Id
+    @Column(name= "id_item", nullable = false)
+    private Integer idItem;
+    @Id
+    @Column(name="id_order", nullable = false)
+    private Integer idOrder;
+    @Column(name="id_pizza", nullable = false)
+    private Integer idPizza;
+    @Column(nullable = false, columnDefinition = "DECIMAL(2,1)")
+    private Double quantity;
+    @Column(nullable = false, columnDefinition = "DECIMAL(5,2)")
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_order", referencedColumnName = "id_order", insertable = false, updatable = false)
+    private OrderEntity order;
+
+    @OneToOne
+    @JoinColumn(name = "id_pizza", referencedColumnName = "id_pizza", insertable = false, updatable = false)
+    private PizzaEntity pizza;
+}
