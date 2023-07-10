49f792b88d93a3f11d943f1e59cb7aa0df337dcd
Juan Lara <max_lara3005@hotmail.com>
2023-07-09 19:17:46 -0600

    Agregando los demas eventos de un Crud Repository

--- src/main/java/com/platzi/pizza/web/controller/PizzaController.java
+++ src/main/java/com/platzi/pizza/web/controller/PizzaController.java
@@ -4,10 +4,7 @@ import com.platzi.pizza.persistence.entity.PizzaEntity;
 import com.platzi.pizza.services.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +29,34 @@ public class PizzaController {
     @GetMapping("/{idPizza}")
     public ResponseEntity<PizzaEntity> getById(@PathVariable(name = "idPizza") Integer id){
         return ResponseEntity.ok(this.pizzaService.get(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<PizzaEntity> save(@RequestBody PizzaEntity pizza){
+        if(pizza.getIdPizza() == null || !this.pizzaService.exists(pizza.getIdPizza())) {
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        } else {
+            return  ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping()
+    public ResponseEntity<PizzaEntity> update(@RequestBody PizzaEntity pizza){
+        if(pizza.getIdPizza() != null && this.pizzaService.exists(pizza.getIdPizza())) {
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        } else {
+            return  ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{idPizza}")
+    public  ResponseEntity<Void> delete (@PathVariable(name="idPizza") int idPizza){
+        if(this.pizzaService.exists(idPizza)){
+            this.pizzaService.delete(idPizza);
+            return  ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 
